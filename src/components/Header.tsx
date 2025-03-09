56ecdc06234fdd72f04902f6488231341ae55195
Shiv Pratap <sp09singhwaghel@gmail.com>
2025-03-09 20:05:27 +0530

    add : header for application

---
+++ src/components/Header.tsx
@@ -0,0 +1,7 @@
+export default function Header() {
+  return (
+    <div className="w-full h-16 md:h-20 bg-gray-800 text-white flex items-center justify-center font-bold text-xl sm:text-2xl shadow-md">
+      Personal Finance Visualizer
+    </div>
+  );
+}
