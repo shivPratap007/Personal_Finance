57c71eb8e0f8f9d53f3acf0da66a513f9a7fe929
Shiv Pratap <sp09singhwaghel@gmail.com>
2025-03-09 20:41:54 +0530

    add : header and navment in layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
 import Header from "@/components/Header";
+import Navmenu from "@/components/navmenu";
+import InputExpense from "./input-expense/page";
 
 export default function Home() {
   return (
-    <div className="container max-w-screen min-h-screen bg-gray-300">
-      <Header/>
-      
+    <div className="container max-w-screen min-h-screen">
+      <InputExpense />
     </div>
   );
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,6 @@
 export default function Header() {
   return (
-    <div className="w-full h-16 md:h-20 bg-gray-800 text-white flex items-center justify-center font-bold text-xl sm:text-2xl shadow-md">
+    <div className="w-full h-16 md:h-20 bg-blue-900 text-white flex items-center justify-center font-bold text-xl sm:text-2xl shadow-md">
       Personal Finance Visualizer
     </div>
   );
