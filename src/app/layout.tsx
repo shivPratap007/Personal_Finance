0ad0a399a060be93be26f70b0d175062dd3129a3
Shiv Pratap <sp09singhwaghel@gmail.com>
2025-03-10 20:58:17 +0530

    add : toaster as dependency

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
 import Navmenu from "@/components/navmenu";
+import { Toaster } from "react-hot-toast";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,6 +30,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <Toaster position="top-center" />
         <Header />
         <Navmenu />
         {children}
