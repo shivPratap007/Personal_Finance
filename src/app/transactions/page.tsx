36afd8683bcb8b8a12691fc3d70b5ef8264742d3
Shiv Pratap <sp09singhwaghel@gmail.com>
2025-03-11 12:55:14 +0530

    add : buttons to see the bar chart and list transactions

--- src/app/transactions/page.tsx
+++ src/app/transactions/page.tsx
@@ -10,7 +10,7 @@ import {
 } from "@/components/ui/dialog";
 import { X } from "lucide-react";
 import EditForm from "@/components/EditForm";
-
+import MonthlyBarChart from "@/components/MonthlyBarChart";
 
 export type Ttransaction = {
   _id: number;
@@ -21,12 +21,12 @@ export type Ttransaction = {
 };
 
 const categoryIcons: Record<string, string> = {
-  Food: "🍕",
-  Transport: "🚌",
-  Shopping: "🛍️",
-  Health: "💊",
-  Entertainment: "🎬",
-  Other: "💸",
+  food: "🍕",
+  rent: "🏠",
+  entertainment: "🎬",
+  transport: "🚌",
+  housing: "🏡",
+  others: "📦",
 };
 
 const fetchTransactions = async (
@@ -101,6 +101,7 @@ export default function Transactions() {
   const [editDetails, setEditDetails] = useState<Ttransaction | null>(null);
   const [isOpen, setIsOpen] = useState(false);
   const [selectedMonth, setSelectedMonth] = useState<number>(0);
+  const [visible, setVisible] = useState(false);
 
   const getTransactions = useCallback(async (selectedMonth: number) => {
     setLoading(true);
@@ -145,71 +146,96 @@ export default function Transactions() {
   return (
     <>
       <div className="flex justify-end my-4 z-10">
-        <select className="border border-gray-300 rounded-md p-2" onChange={(e) => setSelectedMonth(Number(e.target.value))}>
+        <select
+          className="border border-gray-300 rounded-md p-2"
+          onChange={(e) => setSelectedMonth(Number(e.target.value))}
+        >
           <option value="0">All</option>
           {months.map((month) => (
-            <option value={month.value}>{month.name}</option>
+            <option key={month.name} value={month.value}>
+              {month.name}
+            </option>
           ))}
         </select>
       </div>
-      <div className="grid md:grid-cols-3 sm:grid-cols-2 gap-6">
-        {loading ? (
-          <p className="col-span-full text-center text-gray-500 py-8">
-            Loading transactions...
-          </p>
-        ) : transactions.length > 0 ? (
-          transactions.map((transaction) => (
-            <div
-              key={transaction._id}
-              className="bg-white p-6 rounded-xl shadow-md hover:shadow-lg transition transform hover:-translate-y-1"
-            >
-              <div className="flex items-center space-x-4">
-                <span className="text-3xl">
-                  {categoryIcons[transaction.category] || "💰"}
-                </span>
-                <div>
-                  <p className="text-lg font-semibold text-gray-700">
-                    {transaction.description}
+      <div className="flex justify-center gap-2 my-4 z-10">
+        <button
+          className="shadow-md  py-2 px-4 rounded cursor-pointer"
+          onClick={() => setVisible(false)}
+        >
+          Transactions
+        </button>
+        <button
+          className="shadow-md  py-2 px-4 rounded cursor-pointer"
+          onClick={() => setVisible(true)}
+        >
+          Bar Chart
+        </button>
+      </div>
+      {visible ? (
+        <MonthlyBarChart transactions={transactions} />
+      ) : (
+        <div className="grid md:grid-cols-2 lg:grid-cols-3 sm:grid-cols-2 gap-6">
+          {loading ? (
+            <p className="col-span-full text-center text-gray-500 py-8">
+              Loading transactions...
+            </p>
+          ) : transactions.length > 0 ? (
+            transactions.map((transaction) => (
+              <div
+                key={transaction._id}
+                className="bg-white p-6 rounded-xl shadow-md hover:shadow-lg transition transform hover:-translate-y-1"
+              >
+                <div className="flex items-center space-x-4">
+                  <span className="text-3xl">
+                    {categoryIcons[transaction.category] || "💰"}
+                  </span>
+                  <div>
+                    <p className="text-lg font-semibold text-gray-700">
+                      {transaction.description}
+                    </p>
+                    <p className="text-gray-500 text-sm">
+                      {new Intl.DateTimeFormat("en-US", {
+                        year: "numeric",
+                        month: "short",
+                        day: "numeric",
+                      }).format(new Date(transaction.date))}
+                    </p>
+                  </div>
+                </div>
+
+                <div className="mt-4">
+                  <p className="text-xl font-bold text-green-600">
+                    ${transaction.amount}
                   </p>
                   <p className="text-gray-500 text-sm">
-                    {new Intl.DateTimeFormat("en-US", {
-                      year: "numeric",
-                      month: "short",
-                      day: "numeric",
-                    }).format(new Date(transaction.date))}
+                    {transaction.category}
                   </p>
                 </div>
-              </div>
 
-              <div className="mt-4">
-                <p className="text-xl font-bold text-green-600">
-                  ${transaction.amount}
-                </p>
-                <p className="text-gray-500 text-sm">{transaction.category}</p>
+                <div className="mt-4 flex justify-between">
+                  <button
+                    onClick={() => handleEdit(transaction)}
+                    className="px-4 py-2 bg-blue-500 text-white rounded-lg cursor-pointer hover:bg-blue-600 transition"
+                  >
+                    📝 Edit
+                  </button>
+                  <button
+                    onClick={() => handleDelete(transaction._id)}
+                    className="px-4 py-2 bg-red-500 text-white rounded-lg cursor-pointer hover:bg-red-600 transition"
+                  >
+                    🗑 Delete
+                  </button>
+                </div>
               </div>
-
-              <div className="mt-4 flex justify-between">
-                <button
-                  onClick={() => handleEdit(transaction)}
-                  className="px-4 py-2 bg-blue-500 text-white rounded-lg cursor-pointer hover:bg-blue-600 transition"
-                >
-                  📝 Edit
-                </button>
-                <button
-                  onClick={() => handleDelete(transaction._id)}
-                  className="px-4 py-2 bg-red-500 text-white rounded-lg cursor-pointer hover:bg-red-600 transition"
-                >
-                  🗑 Delete
-                </button>
-              </div>
-            </div>
-          ))
-        ) : (
-          <p className="col-span-full text-center text-gray-500 py-8">
-            No transactions found
-          </p>
-        )}
-      </div>
+            ))
+          ) : (
+            <p className="col-span-full text-center text-gray-500 py-8">
+              No transactions found
+            </p>
+          )}
+        </div>
+      )}
 
       <Dialog open={isOpen} onOpenChange={setIsOpen}>
         <DialogContent className="sm:max-w-md md:max-w-lg lg:max-w-xl">
