50c4136da4127b48bfa28215dd7a776fc45db7c5
Shiv Pratap <sp09singhwaghel@gmail.com>
2025-03-11 19:02:02 +0530

    chore : remove unnecessary consoles

--- src/app/api/transactions/route.ts
+++ src/app/api/transactions/route.ts
@@ -16,12 +16,9 @@ export async function POST(req: NextRequest) {
   try {
     await connectMongo();
     const data = await req.json();
-    console.log(data);
     const transaction = await Transaction.create(data);
-    console.log(transaction);
     return NextResponse.json({ transaction }, { status: 201 });
   } catch (error) {
-    console.log(error);
     return NextResponse.json({ error: "Failed to create transaction" }, { status: 500 });
   }
 }
