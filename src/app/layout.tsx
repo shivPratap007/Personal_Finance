6cf78345b188a09aac58b7e6cb7590d6335dea5a
Shiv Pratap <sp09singhwaghel@gmail.com>
2025-03-11 19:01:41 +0530

    update : padding for mobile screen

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -33,7 +33,7 @@ export default function RootLayout({
         <Toaster position="top-center" />
         <Header />
         <Navmenu />
-        <div className="px-10 md:px-20">
+        <div className="px-4 md:px-20">
         {children}
         </div>
       </body>
